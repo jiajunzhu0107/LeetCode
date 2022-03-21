e368fd720403d06ecb4c4e2d62ff09e000ef5445
Jiajun Zhu <zhujiajun0107@foxmail.com>
2022-03-21 07:33:43 -0700

    Update 171StringToIntegerAtoi.py

--- String/171StringToIntegerAtoi.py
+++ String/171StringToIntegerAtoi.py
@@ -7,23 +7,27 @@ class Solution:
         if s1[0] in ['-', '+']:
             sign = s1[0]
             s1 = s1[1:]
-        elif s[0].isnumeric():
+        elif s1[0].isnumeric():
             sign = '+'
         else:
             return 0
         num_str = ''
         index = 0
-        print(s1[0])
-        while (s1[index].isnumeric()) and (index < len(s1)):
+        while index < len(s1) and s1[index].isnumeric() :
             num_str += s1[index]
             index += 1
         if len(num_str) <= 0:
             return 0
         else:
-            return int(sign + num_str)
-
+            if int(sign + num_str) < -2**31:
+                return -2 ** 31
+            elif int(sign + num_str) > 2**31 -1:
+                return 2 **31 -1
+            else:
+                return int(sign + num_str)
+a = -2 **31      
+print(a)
 s = '42'
-print('a'+'b')
 obj = Solution()
 res = obj.myAtoi(s)
 print(f'{res!r}')
